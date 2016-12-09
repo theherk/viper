8b7a78262829983c3c7d332c777b11cf30fd2b8d
Herkermer Sherwood <theherk@gmail.com>
2016-12-09 06:11:09 -0800

    Remove extra comments

--- viper.go
+++ viper.go
@@ -1204,10 +1204,6 @@ func (v *Viper) SafeWriteConfigAs(filename string) error {
 	return v.writeConfig(filename, false)
 }
 
-// WriteConfigKey writes given key and value to file.
-
-// WriteConfigKeyAs writes given key and value to a given filename.
-
 func keyExists(k string, m map[string]interface{}) string {
 	lk := strings.ToLower(k)
 	for mk := range m {
@@ -1330,7 +1326,6 @@ func (v *Viper) unmarshalReader(in io.Reader, c map[string]interface{}) error {
 func marshalWriter(out afero.File, c map[string]interface{}, configType string) error {
 	return v.marshalWriter(out, c, configType)
 }
-
 func (v *Viper) marshalWriter(out afero.File, c map[string]interface{}, configType string) error {
 	return marshalConfigWriter(out, c, configType)
 }
