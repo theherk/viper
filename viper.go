3957146dccee6cf24ba4d54643a2dd69428a5806
Herkermer Sherwood <theherk@gmail.com>
2016-12-15 20:12:53 -0800

    Revert "Modify to only support HCL write in Go 1.7"
    This reverts commit 12b34bc4eb92cbf8ebfd56b79519f448607e3e51.

--- viper.go
+++ viper.go
@@ -35,6 +35,7 @@ import (
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/hashicorp/hcl"
+	"github.com/hashicorp/hcl/hcl/printer"
 	"github.com/magiconair/properties"
 	"github.com/mitchellh/mapstructure"
 	toml "github.com/pelletier/go-toml"
@@ -1302,7 +1303,15 @@ func (v *Viper) marshalWriter(f afero.File, configType string) error {
 		}
 
 	case "hcl":
-		return v.marshalWriterHCL(f)
+		b, err := json.Marshal(c)
+		ast, err := hcl.Parse(string(b))
+		if err != nil {
+			return ConfigMarshalError{err}
+		}
+		err = printer.Fprint(f, ast.Node)
+		if err != nil {
+			return ConfigMarshalError{err}
+		}
 
 	case "prop", "props", "properties":
 		if v.properties == nil {
